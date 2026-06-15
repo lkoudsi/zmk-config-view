673c9049727258ae912a364ba1d71ced9a94640c
lkoudsi <lkoudsi@uwo.ca>
2026-06-15 15:21:27 -0400

    remove current key and update keymap

--- boards/shields/my_nano/widgets/status.c
+++ boards/shields/my_nano/widgets/status.c
@@ -95,7 +95,7 @@ static void draw_top(lv_obj_t *widget, lv_color_t cbuf[], const struct status_st
     //lv_canvas_draw_rect(canvas, 0, 21, 68, 42, &rect_white_dsc);
     //lv_canvas_draw_rect(canvas, 1, 22, 66, 40, &rect_black_dsc);
  
-    lv_canvas_draw_text(canvas, 1, 35, 62, &label_dsc, state->pressed_key);
+    //lv_canvas_draw_text(canvas, 1, 35, 62, &label_dsc, state->pressed_key);
 
     // Rotate canvas
     rotate_canvas(canvas, cbuf);
@@ -285,9 +285,9 @@ ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, laye
 ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
 
 static void set_keycode_status(struct zmk_widget_status *widget, struct keycode_status_state state) {
-    widget->state.pressed_key = zmk_hid_usage_to_string(state.usage_page, state.keycode, state.mods);
+    //widget->state.pressed_key = zmk_hid_usage_to_string(state.usage_page, state.keycode, state.mods);
 
-    draw_top(widget->obj, widget->cbuf, &widget->state);
+    //draw_top(widget->obj, widget->cbuf, &widget->state);
 }
 
 static void keycode_status_update_cb(struct keycode_status_state state) {
